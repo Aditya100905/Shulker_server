f6e07564e899e996e086179a484a17043955fda6
vansh-000 <23165@iiitu.ac.in>
2025-08-14 21:44:33 +0530

    Implement Google authentication

--- src/app.js
+++ src/app.js
@@ -1,32 +1,41 @@
+// app.js
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import './utils/passport.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
 
-// add cors
 app.use(cors({
-    origin: frontendUrl,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
+  origin: frontendUrl,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
 }));
+
 app.use(express.json({ limit: "16kb" }));
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
 import userRouter from './routes/user.routes.js';
 app.use("/api/v1/users", userRouter);
 
