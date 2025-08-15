1304dcdf437c43ed9d36ecc9c29cbbcb6a90e0a9
vansh-000 <23165@iiitu.ac.in>
2025-08-15 20:14:11 +0530

    Refactor AuthSuccess component to improve token handling and logging

---
+++ src/pages/AuthSuccess.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../features/authSlice";
+
+export default function AuthSuccess() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get("accessToken");
+    const refreshToken = params.get("refreshToken");
+    if (accessToken) {
+      fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/current-user`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        credentials: "include",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success && data.message.user) {
+            localStorage.setItem("authToken", accessToken);
+            localStorage.setItem("refreshToken", refreshToken);
+            localStorage.setItem("authUser", JSON.stringify(data.message.user));
+
+            dispatch(login({ user: data.message.user, token: accessToken }));
+            navigate("/dashboard");
+          } else {
+            navigate("/login?error=auth_failed");
+          }
+        });
+    } else {
+      navigate("/login?error=auth_failed");
+    }
+  }, [dispatch, navigate]);
+
+  return <p>Signing you in...</p>;
+}
